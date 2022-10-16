4fd1b6fa70f36892d7a3d835f768bdce397b0046
diversable <dan.mantei@outlook.com>
2022-10-16 10:16:51 -0700

    add projects data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 	console.log(data);
 
-	const { tasks } = data;
+	const { tasks, projects } = data;
 	// END Loading Data
 </script>
 
@@ -16,26 +16,53 @@
 </svelte:head>
 
 <section class="mx-auto">
-	<h1 class="text-2xl underline">Today</h1>
+	<h1 class="text-3xl underline">Today</h1>
 	<form method="POST">
 		<input type="text" name="taskName" />
 		<button type="submit">Submit</button>
 	</form>
 
 	<br /><br /><br />
-	<h2 class="text-xl">Tasks</h2>
-	{#each tasks as { id, taskName, onDays, underHeading, started, finished } (id)}
-		ID: {id}
-		<br />
-		Task Name: {taskName}
-		<br />
-		On Days: {onDays},
-		<br />
-		Under Heading: {underHeading}
-		<br />
-		Started: {started}
-		<br />
-		Finished: {finished}
-		<br /><br /><br />
+	<!-- SHOW TASKS -->
+	<h2 class="sub-heading">Tasks</h2>
+	{#each tasks as { id, taskName, onDays, underHeading, finished } (id)}
+		<p>
+			ID: {id}
+			<br /><br />
+			Task Name: {taskName}
+			<br /><br />
+			On Days: {onDays},
+			<br /><br />
+			Under Heading: {underHeading}
+
+			<br /><br />
+			Finished: {finished}
+			<br /><br /><br />
+		</p>
+	{/each}
+
+	<!-- SHOW PROJECTS -->
+	<h2 class="sub-heading">Projects</h2>
+	{#each projects as { id, taskName, onDays, underHeading, started, finished } (id)}
+		<p>
+			ID: {id}
+			<br /><br />
+			Task Name: {taskName}
+			<br /><br />
+			On Days: {onDays},
+			<br /><br />
+			Under Heading: {underHeading}
+			<br /><br />
+			Started: {started}
+			<br /><br />
+			Finished: {finished}
+			<br /><br /><br />
+		</p>
 	{/each}
 </section>
+
+<style lang="postcss">
+	.sub-heading {
+		@apply text-2xl underline underline-offset-4 text-slate-700;
+	}
+</style>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,12 +5,13 @@ import type { PageLoad } from "./$types";
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
+// Task = thing you can do in 1 sitting
 const tasks = [{
   id: crypto.randomUUID(),
   taskName: "read email",
   onDays: ["Monday", "Wednesday", "Friday"],
   underHeading: "Morning",
-  started: false,
+
   finished: false,
 }, {
   id: crypto.randomUUID(),
@@ -25,13 +26,43 @@ const tasks = [{
     "Sunday",
   ],
   underHeading: "Bedtime Routine",
+
+  finished: false,
+}];
+
+const projects = [{
+  id: crypto.randomUUID(),
+  taskName: "read & do exercises in Rust Web book",
+  onDays: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  underHeading: "Afternoon Productivity Block",
+  started: true,
+  finished: false,
+}, {
+  id: crypto.randomUUID(),
+  taskName: "prep grad school applications",
+  onDays: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  underHeading: "Morning Productivity Block",
   started: false,
   finished: false,
 }];
 
 export const load: PageLoad = ({ data }) => {
   console.log(data);
-  return { tasks: tasks };
+  return { tasks: tasks, projects: projects };
 };
 
 export const actions: Actions = {
